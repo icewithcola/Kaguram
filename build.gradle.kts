53619f63885edfae3e062a0bd1191b628f4ff8cb
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-06-23 03:04:48 +0800

    chore: find build-tools version automatically
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ val verName = if (Version.isStable) {
 val androidTargetSdkVersion by extra(34)
 val androidMinSdkVersion by extra(27)
 val androidCompileSdkVersion by extra(34)
-val androidBuildToolsVersion by extra("34.0.0")
+val androidBuildToolsVersion = Version.findBuildToolsVersion()
 val androidCompileNdkVersion = "26.3.11579264"
 
 fun Project.configureBaseExtension() {
