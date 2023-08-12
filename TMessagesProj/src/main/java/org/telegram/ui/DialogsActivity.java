f7ee99bc7df70c3335d99db4fe92e5bf96850afd
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-08-13 02:26:32 +0800

    feat: optimize hide stories
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -8633,7 +8633,7 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
         }
     }
 
-    public boolean storiesEnabled = true;
+    public boolean storiesEnabled = !Config.hideStories;
     private void updateStoriesPosting() {
         final boolean storiesEnabled = getMessagesController().storiesEnabled();
         if (this.storiesEnabled != storiesEnabled) {
