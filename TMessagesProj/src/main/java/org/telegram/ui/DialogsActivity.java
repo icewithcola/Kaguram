43e9ceb495afa363ea47cfaf8f2b460da97aec31
Next Alone <12210746+NextAlone@users.noreply.github.com>
2024-04-11 17:21:37 +0800

    feat: hide dialogs floating button
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -6947,6 +6947,9 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
         updateVisibleRows(0, false);
         updateProxyButton(false, true);
         updateStoriesVisibility(false);
+        if (Config.hideDialogsFloatingButton) {
+            hideFloatingButton(true);
+        }
         checkSuggestClearDatabase();
     }
 
@@ -11049,6 +11052,9 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
     }
 
     private void hideFloatingButton(boolean hide) {
+        if (Config.hideDialogsFloatingButton) {
+            hide = true;
+        }
         if (rightSlidingDialogContainer.hasFragment()) {
             hide = true;
         }
